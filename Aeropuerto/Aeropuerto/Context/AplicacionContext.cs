91837bcd438cad3043a7ee7bddbe4d7add4ff600
ronaldCervantes <ronaldcervantes31@gmail.com>
2023-12-04 11:19:31 -0400

    subido del modelado

---
+++ Aeropuerto/Aeropuerto/Context/AplicacionContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Aeropuerto.Models;
+
+namespace Aeropuerto.Context
+{
+    public class AplicacionContext : DbContext
+    {
+        public AplicacionContext(DbContextOptions<AplicacionContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<hangares> Hangares { get; set; }
+        public DbSet< aviones> Aviones { get; set; }
+        public DbSet<pilotos > Pilotos { get; set; }
+  
+      
+    }
+}
---
+++ Aeropuerto/Aeropuerto/Models/hangares.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Aeropuerto.Models
+{
+    public class hangares
+
+    {
+        public int HangarId { get; set; }
+        public int capacidad { get; set; }
+        public string localizacion { get; set; }
+        
+        
+
+    }
+}
